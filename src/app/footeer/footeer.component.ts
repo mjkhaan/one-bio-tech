bb86dc6a96524af1b7193b8193af4216d78cab45
Atif <atif.taskeen92@gmail.com>
2024-07-30 22:58:12 +0500

    add changes

--- src/app/footeer/footeer.component.ts
+++ src/app/footeer/footeer.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footeer',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './footeer.component.html',
   styleUrl: './footeer.component.scss'
 })
 export class FooteerComponent {
-
+  currentRoute: string = ''
+  constructor(private router: Router, private renderer: Renderer2) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url;
+      }
+    });
+  }
 }
