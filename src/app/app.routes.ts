2816db89930386e183e1cb930070906729fe9c92
Muhammad Junaid Khan <mjunaid.khan@octdaily.com>
2025-03-18 04:22:44 +0500

    Adding Privacy Policy Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
 
 export const routes: Routes = [
   {
@@ -15,4 +16,10 @@ export const routes: Routes = [
     component: ContactComponent,
     title: 'Apollo Technologies | Contact'
   },
+  {
+    path: 'privacy-policy',
+    pathMatch: 'full',
+    component: PrivacyComponent,
+    title: 'Apollo Technologies | Privacy Policy'
+  }
 ];
