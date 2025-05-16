3708d16546ae7b60c2a47170ae09e8489870f98c
Muhammad Junaid Khan <mjunaid.khan@octdaily.com>
2025-05-16 21:42:39 +0500

    Why us and our products pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { WhyUsComponent } from './pages/why-us/why-us.component';
+import { OurProductsComponent } from './pages/our-products/our-products.component';
 
 export const routes: Routes = [
   {
@@ -7,5 +9,17 @@ export const routes: Routes = [
     pathMatch: 'full',
     component: HomeComponent,
     title: 'OneBioTech | Home'
+  },
+  {
+    path: 'why-us',
+    pathMatch: 'full',
+    component: WhyUsComponent,
+    title: 'OneBioTech | Why Us'
+  },
+  {
+    path: 'our-products',
+    pathMatch: 'full',
+    component: OurProductsComponent,
+    title: 'OneBioTech | Our Products'
   }
 ];
